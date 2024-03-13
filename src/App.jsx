e80a01c15e6fa46e1a1363054cc3361f5f681ad3
Alexandr <aiprivet@icloud.com>
2024-03-14 00:01:20 +0300

    create add-children fn

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,9 @@ export default function App() {
   const tree = [
     {
       type: "div",
-      styles: ["bg-black"],
-      id: 361,
-      childrens: [
-        {
-          type: "div",
-          styles: ["bg-red-500", "w-1/2"],
-          id: 400,
-          childrens: [
-            {
-              type: "h1",
-              styles: ["text-blue-500", "text-xl"],
-              content: "Hello world!",
-              id: 121,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      type: "div",
-      styles: ["bg-green-500"],
-      id: 11,
-      childrens: [
-        {
-          type: "h1",
-          styles: ["text-black", "text-3xl"],
-          content: "Goodbye world!",
-          id: 154,
-        },
-      ],
+      styles: ["bg-black h-24 w-screen"],
+      id: 0,
+      childrens: [],
     },
   ];
 
@@ -44,11 +17,27 @@ export default function App() {
   const [page, setPage] = useState(tree);
   const [selected, setSelected] = useState(0);
 
-  
   function handleAdd(type, styles, content = "") {
     let block = createBlock(type, styles.split(" "), content);
     let newTree = [...page, block];
     setPage(newTree);
+  }
+
+  function addChidlren(foundId, children) {
+    let tree = JSON.parse(JSON.stringify(page));
+    function addToChildrenById() {
+      for (const node of tree) {
+        if (node.id === foundId) {
+          node.childrens.push(children);
+          return;
+        }
+        if (node.childrens.length > 0) {
+          addToChildrenById(node.childrens);
+        }
+      }
+    }
+    addToChildrenById();
+    setPage(() => tree);
   }
 
   function createBlock(type, styles, content) {
@@ -81,7 +70,11 @@ export default function App() {
               }}
               id={node.id}
               key={node.id}
-              className={`${node.styles.join(" ")} ${selected === node.id ? 'border border-blue-500 border-dashed' : ''}`}
+              className={`${node.styles.join(" ")} ${
+                selected === node.id
+                  ? "border border-blue-500 border-dashed"
+                  : ""
+              }`}
             >
               {parseTree(node.childrens)}
             </div>
@@ -107,7 +100,9 @@ export default function App() {
             }}
             id={node.id}
             key={node.id}
-            className={`${node.styles.join(" ")} ${selected === node.id ? 'border border-blue-500 border-dashed' : ''}`}
+            className={`${node.styles.join(" ")} ${
+              selected === node.id ? "border border-blue-500 border-dashed" : ""
+            }`}
           >
             {node.content}
           </h1>
@@ -123,44 +118,88 @@ export default function App() {
 
       <div className="fixed right-8 bottom-8">
         <div className="border border-black rounded-xl p-8 z-50 bg-white">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleAdd(type, styles, content);
-            }}
-            className="flex flex-col gap-2"
-          >
-            <h1 className="text-center ">Добавить элемент</h1>
-            <label>Тип элемента</label>
-            <select
-              value={type}
-              onChange={(e) => setType(e.target.value)}
-              className="border border-black rounded-lg "
+          {selected === 0 ? (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleAdd(type, styles, content);
+              }}
+              className="flex flex-col gap-2"
             >
-              <option value={"div"}>DIV</option>
-              <option value={"h1"}>H1</option>
-            </select>
-            <label>Стили</label>
-            <input
-              onChange={(e) => setStyle(e.target.value)}
-              value={styles}
-              type="text"
-              className="border border-black rounded-lg"
-            />
-            <label>Контент?</label>
-            <input
-              onChange={(e) => setContent(e.target.value)}
-              value={content}
-              type="text"
-              className="border border-black rounded-lg"
-            />
-            <button
-              type="submit"
-              className="border border-black  rounded-xl mt-4 "
+              <h1 className="text-center ">Добавить элемент</h1>
+              <label>Тип элемента</label>
+              <select
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+                className="border border-black rounded-lg "
+              >
+                <option value={"div"}>DIV</option>
+                <option value={"h1"}>H1</option>
+              </select>
+              <label>Стили</label>
+              <input
+                onChange={(e) => setStyle(e.target.value)}
+                value={styles}
+                type="text"
+                className="border border-black rounded-lg"
+              />
+              <label>Контент?</label>
+              <input
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+                type="text"
+                className="border border-black rounded-lg"
+              />
+              <button
+                type="submit"
+                className="border border-black  rounded-xl mt-4 "
+              >
+                Add
+              </button>
+            </form>
+          ) : (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                addChidlren(
+                  selected,
+                  createBlock(type, styles.split(" "), content)
+                );
+              }}
+              className="flex flex-col gap-2"
             >
-              Add
-            </button>
-          </form>
+              <h1 className="text-center ">Добавить потомка</h1>
+              <label>Тип элемента</label>
+              <select
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+                className="border border-black rounded-lg "
+              >
+                <option value={"div"}>DIV</option>
+                <option value={"h1"}>H1</option>
+              </select>
+              <label>Стили</label>
+              <input
+                onChange={(e) => setStyle(e.target.value)}
+                value={styles}
+                type="text"
+                className="border border-black rounded-lg"
+              />
+              <label>Контент?</label>
+              <input
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+                type="text"
+                className="border border-black rounded-lg"
+              />
+              <button
+                type="submit"
+                className="border border-black  rounded-xl mt-4 "
+              >
+                Add
+              </button>
+            </form>
+          )}
         </div>
       </div>
     </div>
